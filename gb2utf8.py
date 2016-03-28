e1552c683579c4f8fe1dda4e856031dc38b92820
heshenghuan <heshenghuan@sina.com>
2016-03-28 14:18:31 +0800

    GB2312 to UTF8

--- gb2utf8.py
+++ gb2utf8.py
@@ -27,17 +27,17 @@ def encode_utf8(src_file, out_file):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) <= 1:
+    if len(sys.argv) < 2:
         print "Not enough parameters."
         print "Usage: python gb2utf8.py src_file [out_file]"
         print "       out_file: if out_file has not been set, the output file"
         print "                 will be save as src_file.utf8"
         sys.exit(0)
-    src_file = sys.argv[0]
-    if len(sys.argv) == 1:
-        out_file = sys.argv[0] + ".utf8"
+    src_file = sys.argv[1]
+    if len(sys.argv) == 2:
+        out_file = sys.argv[1] + ".utf8"
     else:
-        out_file = sys.argv[1]
+        out_file = sys.argv[2]
 
     encode_utf8(src_file, out_file)
     print "UTF8 enconding done for:%s" % src_file
